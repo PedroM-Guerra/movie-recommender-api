53c6007bbfbff442735bb61cd70b38fe98a04436
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-09-25 13:11:50 -0300

    fix: algorithm efficience

--- ga/mygenetic.py
+++ ga/mygenetic.py
@@ -29,57 +29,54 @@ class MyGeneticAlgorithm(Algorithm):
         self.db = db
         self.all_ids = all_ids
         self.query_search = query_search
+
+        # User data
+        rated_movies = RatingsRepository.find_by_userid(self.db, self.query_search)
+        self.watched_movies = [movie.movie.movieId for movie in rated_movies] # Watched movies IDs
+
+        least_liked_genres = set()
+        most_liked_genres = set()
+
+        for m in rated_movies:
+            genres = m.movie.genres.split('|')
+            if m.rating <= 2:
+                least_liked_genres.update(genres)
+            elif m.rating >= 4:
+                most_liked_genres.update(genres)
+
+        # Get the top 3 most and least liked genres
+        top_ll_genres = Counter(least_liked_genres).most_common(3)
+        top_ml_genres = Counter(most_liked_genres).most_common(3)
+
+        self.least_liked_genres_list = [genre for genre, count in top_ll_genres]
+        self.most_liked_genres_list = [genre for genre, count in top_ml_genres]
+
         
     def evaluate(self, individual):
 
-        if len(individual) != len(set(individual)):
+        if len(individual) != len(set(individual)) or set(individual) - set(self.all_ids):
             return (0.0, )
-        
-        if len(list(set(individual) - set(self.all_ids))) > 0:
-            return (0.0, )
-
-        # Dados filmes na lista individual
-        ratings_movies = RatingsRepository.find_by_movieid_list(self.db, individual)
-
-        # User data
-        rated_movies = RatingsRepository.find_by_userid(self.db, 1)
-        watched_movies = [movie.movie.movieId for movie in rated_movies] # Watched movies IDs
-
-        least_liked_genres_list = [genre for m in rated_movies if m.rating <= 2 for genre in m.movie.genres.split('|')]
-        most_liked_genres_list = [genre for m in rated_movies if m.rating >= 4 for genre in m.movie.genres.split('|')]
-
-        l_genre_counts = Counter(least_liked_genres_list)
-        m_genre_counts = Counter(most_liked_genres_list)
-
-        top_ll_genres = l_genre_counts.most_common(3)
-        top_ml_genres = m_genre_counts.most_common(3)
-
-        least_liked_genres_list = [genre for genre, count in top_ll_genres]
-        most_liked_genres_list = [genre for genre, count in top_ml_genres]
 
         points = []
         
         for title in individual:
             current_title = MovieRepository.find_by_id(self.db, title)
+            current_title_genres = set(current_title.genres.split('|'))
             
-            if title in watched_movies:
-                if set(current_title.genres.split('|')).intersection(set(least_liked_genres_list)):
+            if title in self.watched_movies:
+                if current_title_genres.intersection(set(self.least_liked_genres_list)):
                     points.append(-10)
-                elif set(current_title.genres.split('|')).intersection(set(most_liked_genres_list)):
+                elif current_title_genres.intersection(set(self.most_liked_genres_list)):
                     points.append(70)
                 else:
                     points.append(60)
             else:
-                if set(current_title.genres.split('|')).intersection(set(least_liked_genres_list)):
+                if current_title_genres.intersection(set(self.least_liked_genres_list)):
                     points.append(0)
-                elif set(current_title.genres.split('|')).intersection(set(most_liked_genres_list)):
+                elif current_title_genres.intersection(set(self.most_liked_genres_list)):
                     points.append(100)
                 else:
                     points.append(70)
 
-        if len(ratings_movies) > 0:
-            score = sum(points)
-        else:
-            score = 0.0
-
+        score = sum(points) if individual else 0.0
         return (score, )
